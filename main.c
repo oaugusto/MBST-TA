b5cd4ba6c505b03b99e1a1a23e9044cc21ab65cc
oaugusto <oaugusto@outlook.com>
2019-04-19 14:17:39 -0300

    reading file

--- main.c
+++ main.c
@@ -1,36 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 #include "include/graph.h"
 #include "include/median.h"
 #include "include/mbst.h"
 #include "include/distance.h"
 
+typedef struct place {
+    double latitude;
+    double longitude;
+} place; 
+
 int main(int argc, char* argv[]) {
 
-    int graphSize = 5;
+    int n, i, j;
+    double p1, p2;
+
+    FILE *fp;
+    place* cities; //keep cities latitude and longitute
+
+    if (argc < 2) {
+        printf("erro: missing parameters\n");
+        return 0;
+    }
+
+    fp = fopen(argv[1], "r");
+    fscanf(fp, "%d", &n);
+
+    cities = (place*)malloc(n * sizeof(place));
+
+    for (i = 0; i < n; i++) {
+        fscanf(fp, "%lf %lf", &cities[i]->latitude, &cities[i]->longitude);
+    }
     
-    graph* g = makeGraph(graphSize);
+    graph* g = makeGraph(n);
 
-    //0a 1b 2c 3d 4e 5f 6g wikipedia graph
-    insertEdge(g, 0, 1, 7);//ab
-    insertEdge(g, 0, 3, 5);//ad
-    insertEdge(g, 3, 1, 9);//db
-    insertEdge(g, 1, 2, 8);//bc
-    insertEdge(g, 1, 4, 7);//be
-    insertEdge(g, 2, 4, 5);//ce
-    insertEdge(g, 3, 4, 15);//de
-    insertEdge(g, 3, 5, 6);//df
-    insertEdge(g, 4, 5, 8);//ef
-    insertEdge(g, 4, 6, 7);//eg
-    insertEdge(g, 5, 6, 11);//fg
-
+    for (i = n; i >= 0; i--) {
+        for (j = 0; j < i; j++) {
+            insertEdge(g, i, j, distanceEarthKm(cities[i]->latitude, 
+                        cities[i]->longitude, cities[j]->latitude, cities[j]->longitude));
+        }
+    }
+    
     printGraph(g);
     
     printf("\nbottleneck:%f\n", mbst(g));
    
     removeGraph(g);
+
+    free(cities);
 
     return 0;
 }
@@ -58,5 +76,18 @@ int main(int argc, char* argv[]) {
     insertEdge(g, 2, 3, 10);
     insertEdge(g, 3, 4, 10);
     insertEdge(g, 0, 2, 10);
+
+    //0a 1b 2c 3d 4e 5f 6g wikipedia graph
+    insertEdge(g, 0, 1, 7);//ab
+    insertEdge(g, 0, 3, 5);//ad
+    insertEdge(g, 3, 1, 9);//db
+    insertEdge(g, 1, 2, 8);//bc
+    insertEdge(g, 1, 4, 7);//be
+    insertEdge(g, 2, 4, 5);//ce
+    insertEdge(g, 3, 4, 15);//de
+    insertEdge(g, 3, 5, 6);//df
+    insertEdge(g, 4, 5, 8);//ef
+    insertEdge(g, 4, 6, 7);//eg
+    insertEdge(g, 5, 6, 11);//fg
 
 */
