130e2f045b6faac7c703c8436eadace0a6f44b48
alepmaros <alehstk@gmail.com>
2019-04-21 12:33:07 -0300

    Novas entradas e print do output

--- main.c
+++ main.c
@@ -42,9 +42,9 @@ int main(int argc, char* argv[]) {
         }
     }
     
-    printGraph(g);
+    // printGraph(g);
     
-    printf("\nbottleneck:%f\n", mbst(g));
+    printf("%d\n", (int) mbst(g));
    
     removeGraph(g);
 
